6fc0262e6fd44e958b7736517837cf7d10762661
Chandinivilluri <chandiniv.softline@gmail.com>
2025-01-13 10:46:16 +0530

    addition

---
+++ 03_array.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+MOVIES=('pushpa' 'RRR' 'Devara')
+
+echo "First movie is $MOVIES[1]"
+echo "second movie is $MOVIES[2]"
+echo "Third movie is $MOVIES[3]"
+echo "All movies are $MOVIES[@]"
