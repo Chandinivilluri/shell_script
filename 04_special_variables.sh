960c2a9e0e236607890dff604e8b5dbcf3434406
Chandinivilluri <chandiniv.softline@gmail.com>
2025-01-13 11:05:51 +0530

    addition

---
+++ 04_special_variables.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo "Number of variables passed: $#"
+echo "All variables passed: $@"
+echo "present working directory: $PWD"
+echo "current user home directory: $HOME"
+sleep 20&
+echo "which user is running the script: $USER"
+echo "process id of current script: $$"
+sleep 60
+echo "process id of last command which run in background: $!"
+echo "The output of command: $?"
+echo "The scrip name is: $0"
